64663af6d40b8ef0308db95c35cfdf422b552ece
dodoflyawaydodoflyaway <munisc18@gmail.com>
2021-04-16 21:11:29 +0530

    creating html pages

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,'products/home.html')
+
+@login_required
+def create(request):
+    return render(request,'products/create.html')
