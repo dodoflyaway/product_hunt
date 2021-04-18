0481495ce2c6b18fce311e566c3bfa7158000e6b
dodoflyawaydodoflyaway <munisc18@gmail.com>
2021-04-18 13:48:39 +0530

    adding the create panel

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from products import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from  django.utils import timezone
+#from django.contrib.auth.models import User
 
 def home(request):
     return render(request,'products/home.html')
 
 @login_required
 def create(request):
-    return render(request,'products/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'https://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+
+        else:
+            return render(request,'products/create.html',{'error':'All fields are required.'})
+
+    else:
+        return render(request,'products/create.html')
