fee8ae49fff484d92d9ca3bedb9b9649685969d3
dodoflyawaydodoflyaway <munisc18@gmail.com>
2021-04-19 12:49:22 +0530

    made the detail page

--- products/models.py
+++ products/models.py
@@ -11,17 +11,11 @@ class Product(models.Model):
     votes_total = models.IntegerField(default=1)
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+         return self.title
 
+    def summary(self):
+        return self.body[:100] + ' ...'
 
-
-
-
-
-def __str__(self):
-     return self.title
-
-def summary(self):
-    return self.body[:100] + ' ...'
-
-def pub_date_pretty(self):
-    return self.pub_date.strftime('%b %e %Y')
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from  . import views
 urlpatterns = [
 
     path('create',views.create,name='create'),
+    path('<int:product_id>',views.detail,name='detail'),
 
 ]
